8e15fdc0084836f71a05eb598c49954e205c16ed
Davy de Haas <davy.dehaas98@gmail.com>
2020-04-16 14:48:48 +0200

    Feature/dwet 57 (#6)
    * Add HashGenerator
    
    * Add tests

---
+++ AuthenticationService/Helpers/IHashGenerator.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.Tasks;
+
+namespace AuthenticationService.Helpers
+{
+    public interface IHashGenerator
+    {
+        byte[] Salt();
+        byte[] Hash(string plainText, byte[] salt);
+        bool Verify(string plainText, byte[] salt, byte[] hash);
+    }
+}
