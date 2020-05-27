8bb22cffd471f71953eb4969a0689f6c5f142012
Davy de Haas <davy.dehaas98@gmail.com>
2020-05-27 12:05:24 +0200

    DWET-73: Add Sentry

--- UserMicroservice/Exceptions/InvalidPasswordException.cs
+++ UserMicroservice/Exceptions/InvalidPasswordException.cs
@@ -6,7 +6,9 @@ namespace UserMicroservice.Exceptions
     public class InvalidPasswordException : Exception
     {
         public InvalidPasswordException()
-            : base("The password is invalid.")
+            : base("The password is not valid. " +
+                   "Make sure the password is minimal 8 characters long and contains at least: " +
+                   "1 lower case, 1 upper case, 1 number and 1 special character.")
         {
         }
     }
--- UserMicroservice/Repositories/UserRepository.cs
+++ UserMicroservice/Repositories/UserRepository.cs
@@ -9,46 +9,46 @@ namespace UserMicroservice.Repositories
 {
     public class UserRepository : IUserRepository
     {
-        private readonly IMongoCollection<User> _users;
+        private readonly IMongoCollection<User> _userCollection;
 
-        public UserRepository(IDatabaseSettings settings)
+        public UserRepository(IDatabaseSettings databaseSettings)
         {
-            var client = new MongoClient(settings.ConnectionString);
-            var database = client.GetDatabase(settings.DatabaseName);
+            var client = new MongoClient(databaseSettings.ConnectionString);
+            var database = client.GetDatabase(databaseSettings.DatabaseName);
 
-            _users = database.GetCollection<User>(settings.CollectionName);
+            _userCollection = database.GetCollection<User>(databaseSettings.CollectionName);
         }
         
         public async Task<User> CreateAsync(User user)
         {
-            await _users.InsertOneAsync(user);
+            await _userCollection.InsertOneAsync(user);
             
             return user;
         }
 
         public async Task<List<User>> ReadAsync() =>
-            await _users.Find(user => true).ToListAsync();
+            await _userCollection.Find(user => true).ToListAsync();
 
         public async Task<User> ReadByIdAsync(Guid id) =>
-            await _users.Find(user => user.Id == id).FirstOrDefaultAsync();
+            await _userCollection.Find(user => user.Id == id).FirstOrDefaultAsync();
         
         public async Task<User> ReadByUsernameAsync(string username) =>
-            await _users.Find(user => user.Username == username).FirstOrDefaultAsync();
+            await _userCollection.Find(user => user.Username == username).FirstOrDefaultAsync();
         
         public async Task<User> ReadByEmailAsync(string email) =>
-            await _users.Find(user => user.Email == email).FirstOrDefaultAsync();
+            await _userCollection.Find(user => user.Email == email).FirstOrDefaultAsync();
 
         public async Task<User> UpdateAsync(Guid id, User userIn)
         {
-            await _users.ReplaceOneAsync(user => user.Id == id, userIn);
+            await _userCollection.ReplaceOneAsync(user => user.Id == id, userIn);
             
-            return await _users.Find(user => user.Id == id).FirstOrDefaultAsync();
+            return await _userCollection.Find(user => user.Id == id).FirstOrDefaultAsync();
         }
 
         public async Task DeleteByIdAsync(Guid id) =>
-            await _users.DeleteOneAsync(user => user.Id == id);
+            await _userCollection.DeleteOneAsync(user => user.Id == id);
 
         public async Task DeleteByUserAsync(User userIn) =>
-            await _users.DeleteOneAsync(user => user.Id == userIn.Id);
+            await _userCollection.DeleteOneAsync(user => user.Id == userIn.Id);
     }
 }
